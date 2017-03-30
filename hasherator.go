06a420fc7fc78638f79df775a169dae389f3ed04
defrankland <defrankland@gmail.com>
2017-03-29 22:54:28 -0700

    fix tabs, remove idea files

--- hasherator.go
+++ hasherator.go
@@ -1,120 +1,120 @@
 package hasherator
 
 import (
-	"crypto/md5"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-	"strings"
+  "crypto/md5"
+  "fmt"
+  "io"
+  "io/ioutil"
+  "os"
+  "strings"
 )
 
 type AssetsDir struct {
-	Map map[string]string
+  Map map[string]string
 }
 
 var noHashDirList *[]string
 
 func (a *AssetsDir) Run(sourcePath, workingPath string, noHashDirs []string) error {
 
-	noHashDirList = &noHashDirs
+  noHashDirList = &noHashDirs
 
-	a.Map = map[string]string{}
+  a.Map = map[string]string{}
 
-	err := os.RemoveAll(workingPath)
-	if err != nil {
-		return fmt.Errorf("failed to remove working directory prior to copy: " + err.Error())
-	}
+  err := os.RemoveAll(workingPath)
+  if err != nil {
+    return fmt.Errorf("failed to remove working directory prior to copy: " + err.Error())
+  }
 
-	err = a.recursiveHashAndCopy(sourcePath, workingPath)
-	if err != nil {
-		return err
-	}
+  err = a.recursiveHashAndCopy(sourcePath, workingPath)
+  if err != nil {
+    return err
+  }
 
-	return nil
+  return nil
 }
 
 func (a *AssetsDir) recursiveHashAndCopy(dirPath, runtimePath string) error {
-	var err error
+  var err error
 
-	assetDirs, err := ioutil.ReadDir(dirPath)
-	if err != nil {
-		return fmt.Errorf("error reading directory: %s", err)
-	}
+  assetDirs, err := ioutil.ReadDir(dirPath)
+  if err != nil {
+    return fmt.Errorf("error reading directory: %s", err)
+  }
 
-	for _, fileEntry := range assetDirs {
+  for _, fileEntry := range assetDirs {
 
-		entryName := fileEntry.Name()
+    entryName := fileEntry.Name()
 
-		if fileEntry.IsDir() {
-			newPath := fmt.Sprintf("%s/%s", runtimePath, entryName)
+    if fileEntry.IsDir() {
+      newPath := fmt.Sprintf("%s/%s", runtimePath, entryName)
 
-			err := os.MkdirAll(newPath, 0777)
-			if err != nil {
-				panic("failed to make directory: " + err.Error())
-			}
+      err := os.MkdirAll(newPath, 0777)
+      if err != nil {
+        panic("failed to make directory: " + err.Error())
+      }
 
-			err = a.recursiveHashAndCopy(fmt.Sprintf("%s%s/", dirPath, fileEntry.Name()), newPath)
-			if err != nil {
-				return err
-			}
+      err = a.recursiveHashAndCopy(fmt.Sprintf("%s%s/", dirPath, fileEntry.Name()), newPath)
+      if err != nil {
+        return err
+      }
 
-		} else {
+    } else {
 
-			var fileExtension string
-			var dot string
-			if strings.Contains(entryName, ".") {
-				fileExtension = entryName[strings.LastIndex(entryName, ".")+1:]
-				entryName = entryName[:strings.LastIndex(entryName, ".")]
-				dot = "."
-			}
+      var fileExtension string
+      var dot string
+      if strings.Contains(entryName, ".") {
+        fileExtension = entryName[strings.LastIndex(entryName, ".")+1:]
+        entryName = entryName[:strings.LastIndex(entryName, ".")]
+        dot = "."
+      }
 
-			file, err := ioutil.ReadFile(fmt.Sprintf("%s%s", dirPath, fileEntry.Name()))
-			if err != nil {
-				return fmt.Errorf("failed to read file: " + err.Error())
-			}
+      file, err := ioutil.ReadFile(fmt.Sprintf("%s%s", dirPath, fileEntry.Name()))
+      if err != nil {
+        return fmt.Errorf("failed to read file: " + err.Error())
+      }
 
-			var hash string
-			var noHash bool
-			dir := strings.Split(runtimePath, "/")
-			for _, noDir := range *noHashDirList {
-				if noDir == dir[len(dir)-1] {
-					noHash = true
-				}
-			}
+      var hash string
+      var noHash bool
+      dir := strings.Split(runtimePath, "/")
+      for _, noDir := range *noHashDirList {
+        if noDir == dir[len(dir)-1] {
+          noHash = true
+        }
+      }
 
-			if !noHash {
-				h := md5.Sum(file)
-				hash = fmt.Sprintf("-%x", string(h[:16]))
-			}
+      if !noHash {
+        h := md5.Sum(file)
+        hash = fmt.Sprintf("-%x", string(h[:16]))
+      }
 
-			err = copyFile(fmt.Sprintf("%s%s", dirPath, fileEntry.Name()),
-				fmt.Sprintf("%s/%s%s%s%s", runtimePath, entryName, hash, dot, fileExtension))
-			if err != nil {
-				return fmt.Errorf("failed to return file: " + err.Error())
-			}
+      err = copyFile(fmt.Sprintf("%s%s", dirPath, fileEntry.Name()),
+        fmt.Sprintf("%s/%s%s%s%s", runtimePath, entryName, hash, dot, fileExtension))
+      if err != nil {
+        return fmt.Errorf("failed to return file: " + err.Error())
+      }
 
-			a.Map[fileEntry.Name()] = fmt.Sprintf("%s%s%s%s", entryName, hash, dot, fileExtension)
-		}
-	}
-	return nil
+      a.Map[fileEntry.Name()] = fmt.Sprintf("%s%s%s%s", entryName, hash, dot, fileExtension)
+    }
+  }
+  return nil
 }
 
 func copyFile(src, dst string) error {
 
-	in, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
+  in, err := os.Open(src)
+  if err != nil {
+    return err
+  }
+  defer in.Close()
 
-	out, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
+  out, err := os.Create(dst)
+  if err != nil {
+    return err
+  }
+  defer out.Close()
 
-	_, err = io.Copy(out, in)
+  _, err = io.Copy(out, in)
 
-	return err
+  return err
 }
--- hasherator_test.go
+++ hasherator_test.go
@@ -1,188 +1,188 @@
 package hasherator
 
 import (
-	"crypto/md5"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strings"
+  "crypto/md5"
+  "fmt"
+  "io/ioutil"
+  "os"
+  "strings"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
 )
 
 var _ = Describe("Hasherator", func() {
-	var (
-		hashDir   string = "./test/hashed/assets/"
-		sourceDir string = "./test/assets/"
-		assets    AssetsDir
-	)
+  var (
+    hashDir   string = "./test/hashed/assets/"
+    sourceDir string = "./test/assets/"
+    assets    AssetsDir
+  )
 
-	BeforeEach(func() {
-		assets = AssetsDir{}
-		err := assets.Run(sourceDir, hashDir, []string{"donothash", "donthashthiscssman", "thisshouldnotbehashedtoo"})
+  BeforeEach(func() {
+    assets = AssetsDir{}
+    err := assets.Run(sourceDir, hashDir, []string{"donothash", "donthashthiscssman", "thisshouldnotbehashedtoo"})
 
-		Expect(err).To(BeNil())
-	})
+    Expect(err).To(BeNil())
+  })
 
-	Context("when hashed directory is created", func() {
-		var hashDir []os.FileInfo
-		var err error
+  Context("when hashed directory is created", func() {
+    var hashDir []os.FileInfo
+    var err error
 
-		BeforeEach(func() {
-			hashDir, err = ioutil.ReadDir("./test")
-		})
+    BeforeEach(func() {
+      hashDir, err = ioutil.ReadDir("./test")
+    })
 
-		It("does not return an error", func() {
-			Expect(err).ToNot(HaveOccurred())
-		})
+    It("does not return an error", func() {
+      Expect(err).ToNot(HaveOccurred())
+    })
 
-		It("creates a hashed directories", func() {
-			Expect(hashDir[1].Name()).To(Equal("hashed"))
-		})
-	})
+    It("creates a hashed directories", func() {
+      Expect(hashDir[1].Name()).To(Equal("hashed"))
+    })
+  })
 
-	Context("when files are hashed", func() {
-		var src []os.FileInfo
-		var hashed []os.FileInfo
-		var err error
+  Context("when files are hashed", func() {
+    var src []os.FileInfo
+    var hashed []os.FileInfo
+    var err error
 
-		Context("when dohash directory is hashed", func() {
-			BeforeEach(func() {
-				hashed, err = ioutil.ReadDir(hashDir + "/dohash")
-				src, err = ioutil.ReadDir(sourceDir + "/dohash")
-			})
+    Context("when dohash directory is hashed", func() {
+      BeforeEach(func() {
+        hashed, err = ioutil.ReadDir(hashDir + "/dohash")
+        src, err = ioutil.ReadDir(sourceDir + "/dohash")
+      })
 
-			It("moves all files to the same place within the hashed directory", func() {
+      It("moves all files to the same place within the hashed directory", func() {
 
-				for i := 0; i < len(src); i++ {
-					srcName := src[i].Name()
-					if strings.Contains(srcName, ".") {
-						srcName = srcName[:strings.LastIndex(srcName, ".")]
-					}
-					Expect(hashed[i].Name()).To(ContainSubstring(srcName))
-				}
-			})
+        for i := 0; i < len(src); i++ {
+          srcName := src[i].Name()
+          if strings.Contains(srcName, ".") {
+            srcName = srcName[:strings.LastIndex(srcName, ".")]
+          }
+          Expect(hashed[i].Name()).To(ContainSubstring(srcName))
+        }
+      })
 
-			It("does not append a period on a file with no extension", func() {
+      It("does not append a period on a file with no extension", func() {
 
-				for i := 0; i < len(src); i++ {
-					if !strings.Contains(src[i].Name(), ".") {
-						Expect(hashed[i].Name()).ToNot(ContainSubstring("."))
-					}
-				}
-			})
+        for i := 0; i < len(src); i++ {
+          if !strings.Contains(src[i].Name(), ".") {
+            Expect(hashed[i].Name()).ToNot(ContainSubstring("."))
+          }
+        }
+      })
 
-			It("adds the hash string to the file namein dohash", func() {
-				for i := 0; i < len(src); i++ {
+      It("adds the hash string to the file namein dohash", func() {
+        for i := 0; i < len(src); i++ {
 
-					file, _ := ioutil.ReadFile(fmt.Sprintf("%s%s", sourceDir+"/dohash/", src[i].Name()))
-					h := md5.Sum(file)
-					hash := fmt.Sprintf("-%x", string(h[:16]))
+          file, _ := ioutil.ReadFile(fmt.Sprintf("%s%s", sourceDir+"/dohash/", src[i].Name()))
+          h := md5.Sum(file)
+          hash := fmt.Sprintf("-%x", string(h[:16]))
 
-					if !src[i].IsDir() {
-						Expect(hashed[i].Name()).To(ContainSubstring(hash))
-						Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
-					}
-				}
-			})
-		})
+          if !src[i].IsDir() {
+            Expect(hashed[i].Name()).To(ContainSubstring(hash))
+            Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
+          }
+        }
+      })
+    })
 
-		Context("when badboyjones directory is hashed", func() {
-			BeforeEach(func() {
-				hashed, err = ioutil.ReadDir(hashDir + "/dohash/badboyjones")
-				src, err = ioutil.ReadDir(sourceDir + "/dohash/badboyjones")
-			})
+    Context("when badboyjones directory is hashed", func() {
+      BeforeEach(func() {
+        hashed, err = ioutil.ReadDir(hashDir + "/dohash/badboyjones")
+        src, err = ioutil.ReadDir(sourceDir + "/dohash/badboyjones")
+      })
 
-			It("moves all files to the same place within the hashed directory", func() {
+      It("moves all files to the same place within the hashed directory", func() {
 
-				for i := 0; i < len(src); i++ {
-					srcName := src[i].Name()
-					if strings.Contains(srcName, ".") {
-						srcName = srcName[:strings.LastIndex(srcName, ".")]
-					}
-					Expect(hashed[i].Name()).To(ContainSubstring(srcName))
-					Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
-				}
-			})
+        for i := 0; i < len(src); i++ {
+          srcName := src[i].Name()
+          if strings.Contains(srcName, ".") {
+            srcName = srcName[:strings.LastIndex(srcName, ".")]
+          }
+          Expect(hashed[i].Name()).To(ContainSubstring(srcName))
+          Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
+        }
+      })
 
-			It("adds the hash string to the file namein badboyjones", func() {
-				for i := 0; i < len(src); i++ {
+      It("adds the hash string to the file namein badboyjones", func() {
+        for i := 0; i < len(src); i++ {
 
-					file, _ := ioutil.ReadFile(fmt.Sprintf("%s%s", sourceDir+"/dohash/badboyjones/", src[i].Name()))
-					h := md5.Sum(file)
-					hash := fmt.Sprintf("-%x", string(h[:16]))
+          file, _ := ioutil.ReadFile(fmt.Sprintf("%s%s", sourceDir+"/dohash/badboyjones/", src[i].Name()))
+          h := md5.Sum(file)
+          hash := fmt.Sprintf("-%x", string(h[:16]))
 
-					if !src[i].IsDir() {
-						Expect(hashed[i].Name()).To(ContainSubstring(hash))
-						Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
-					}
-				}
+          if !src[i].IsDir() {
+            Expect(hashed[i].Name()).To(ContainSubstring(hash))
+            Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
+          }
+        }
 
-			})
+      })
 
-			It("does not append a period on a file with no extension", func() {
+      It("does not append a period on a file with no extension", func() {
 
-				for i := 0; i < len(src); i++ {
-					if !strings.Contains(src[i].Name(), ".") {
-						Expect(hashed[i].Name()).ToNot(ContainSubstring("."))
-						Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
-					}
-				}
-			})
-		})
+        for i := 0; i < len(src); i++ {
+          if !strings.Contains(src[i].Name(), ".") {
+            Expect(hashed[i].Name()).ToNot(ContainSubstring("."))
+            Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
+          }
+        }
+      })
+    })
 
-	})
+  })
 
-	Context("when files are not hashed", func() {
-		var src []os.FileInfo
-		var hashed []os.FileInfo
-		var err error
+  Context("when files are not hashed", func() {
+    var src []os.FileInfo
+    var hashed []os.FileInfo
+    var err error
 
-		Context("when donothash directory is not hashed", func() {
-			BeforeEach(func() {
-				hashed, err = ioutil.ReadDir(hashDir + "/donothash")
-				src, err = ioutil.ReadDir(sourceDir + "/donothash")
-			})
+    Context("when donothash directory is not hashed", func() {
+      BeforeEach(func() {
+        hashed, err = ioutil.ReadDir(hashDir + "/donothash")
+        src, err = ioutil.ReadDir(sourceDir + "/donothash")
+      })
 
-			It("moves all files to the same place with same names", func() {
-				for i := 0; i < len(src); i++ {
-					Expect(hashed[i].Name()).To(Equal(src[i].Name()))
-					if !src[i].IsDir() {
-						Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
-					}
-				}
-			})
-		})
+      It("moves all files to the same place with same names", func() {
+        for i := 0; i < len(src); i++ {
+          Expect(hashed[i].Name()).To(Equal(src[i].Name()))
+          if !src[i].IsDir() {
+            Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
+          }
+        }
+      })
+    })
 
-		Context("when donthashthiscssman directory is not hashed", func() {
-			BeforeEach(func() {
-				hashed, err = ioutil.ReadDir(hashDir + "/dohash/donthashthiscssman")
-				src, err = ioutil.ReadDir(sourceDir + "/dohash/donthashthiscssman")
-			})
+    Context("when donthashthiscssman directory is not hashed", func() {
+      BeforeEach(func() {
+        hashed, err = ioutil.ReadDir(hashDir + "/dohash/donthashthiscssman")
+        src, err = ioutil.ReadDir(sourceDir + "/dohash/donthashthiscssman")
+      })
 
-			It("moves all files to the same place with same names", func() {
+      It("moves all files to the same place with same names", func() {
 
-				for i := 0; i < len(src); i++ {
-					Expect(hashed[i].Name()).To(Equal(src[i].Name()))
-					Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
-				}
-			})
-		})
+        for i := 0; i < len(src); i++ {
+          Expect(hashed[i].Name()).To(Equal(src[i].Name()))
+          Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
+        }
+      })
+    })
 
-		Context("when thisshouldnotbehashedtoo directory is not hashed", func() {
-			BeforeEach(func() {
-				hashed, err = ioutil.ReadDir(hashDir + "/dohash/thisshouldnotbehashedtoo")
-				src, err = ioutil.ReadDir(sourceDir + "/dohash/thisshouldnotbehashedtoo")
-			})
+    Context("when thisshouldnotbehashedtoo directory is not hashed", func() {
+      BeforeEach(func() {
+        hashed, err = ioutil.ReadDir(hashDir + "/dohash/thisshouldnotbehashedtoo")
+        src, err = ioutil.ReadDir(sourceDir + "/dohash/thisshouldnotbehashedtoo")
+      })
 
-			It("moves all files to the same place with same names", func() {
+      It("moves all files to the same place with same names", func() {
 
-				for i := 0; i < len(src); i++ {
-					Expect(hashed[i].Name()).To(Equal(src[i].Name()))
-					Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
-				}
-			})
-		})
-	})
+        for i := 0; i < len(src); i++ {
+          Expect(hashed[i].Name()).To(Equal(src[i].Name()))
+          Expect(assets.Map[src[i].Name()]).To(Equal(hashed[i].Name()))
+        }
+      })
+    })
+  })
 })
